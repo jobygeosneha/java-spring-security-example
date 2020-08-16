45047259e5686035b34d6e1d0c6cd5c76d3737ee
Ioram Gordadze <Yoh0xFF@gmail.com>
2020-08-16 12:24:57 +0400

    Tiny fixes.

--- src/test/java/io/example/AppRunner.java
+++ src/test/java/io/example/AppRunner.java
@@ -1,23 +1,12 @@
 package io.example;
 
-import io.example.domain.model.Author;
-import io.example.repository.AuthorRepo;
-import io.example.service.BookService;
-import org.bson.types.ObjectId;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AppRunner {
 
-    @Autowired private BookService bookService;
-    @Autowired private AuthorRepo authorRepo;
-
-    @Test
+    // @Test
     public void run() {
-        Author author = authorRepo.getById(new ObjectId("5f12d86ccc5b6963a5a55002"));
-        System.out.println(author);
     }
 
 }
